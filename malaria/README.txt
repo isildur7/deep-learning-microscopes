fb06acddab93628f77a5cf86595345adc55b9646
Amey Chaware <42990935+isildur7@users.noreply.github.com>
2018-11-18 22:48:59 -0500

    Add files train.py and README via upload

---
+++ malaria/README.txt
@@ -0,0 +1,2 @@
+To train, run train.py with proper paths
+To evaluate, run evaluate.py with proper paths
---
+++ malaria/train.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 18:02:31 2018
+
+@author: Amey Chaware
+
+Train the model"""
+#%%
+import argparse
+import logging
+import os
+import random
+
+import tensorflow as tf
+
+from model.input_fn import load_data_malaria
+from model.input_fn import get_iter_from_raw
+from model.utils import Params
+from model.utils import set_logger
+from model.utils import save_dict_to_json
+from model.model_fn import model_fn
+from model.training import train_and_evaluate
+#%%
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_dir', default='C:/Users/ameyc/Documents/deep-learning-microscopes/malaria/experiments/8_bn2/',
+                    help="Experiment directory containing params.json")
+parser.add_argument('--data_dir', default='C:/Users/ameyc/Documents/deep-learning-microscopes/malaria/data/',
+                    help="Directory containing the dataset")
+parser.add_argument('--restore_from', default = None, #default='C:/Users/ameyc/Documents/deep-learning-microscopes/malaria/experiments/test/best_weights',
+                    help="Subdirectory of model dir or file containing the weights")
+#%%
+if __name__ == '__main__':
+    # Set the random seed for the whole graph for reproductible experiments
+    tf.set_random_seed(230)
+
+    # Load the parameters from json file
+    args = parser.parse_args()
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = Params(json_path)
+#%%
+    # Check that we are not overwriting some previous experiment
+    # Comment these lines if you are developing your model and don't care about overwritting
+    #model_dir_has_best_weights = os.path.isdir(os.path.join(args.model_dir, "best_weights"))
+    #overwritting = model_dir_has_best_weights and args.restore_from is None
+    #assert not overwritting, "Weights found in model_dir, aborting to avoid overwrite"
+
+    # Set the logger
+    set_logger(os.path.join(args.model_dir, 'train.log'))
+
+    # Create the input data pipeline
+    logging.info("Creating the datasets...")
+    data_dir = args.data_dir
+    #train_data_dir = os.path.join(data_dir, "train_signs")
+    #dev_data_dir = os.path.join(data_dir, "dev_signs")
+    
+    # Create the two iterators over the two datasets
+    data = load_data_malaria(data_dir, params)
+    train_inputs = get_iter_from_raw(data, params, numsuper = params.numsuper, intensity_scale = 1/params.numsuper, training = True)  
+    eval_inputs = get_iter_from_raw(data, params, numsuper = params.numsuper, intensity_scale = 1/params.numsuper, training = False) 
+#%%
+    # Specify the sizes of the dataset we train on and evaluate on
+    params.train_size = int(params.number*params.split)
+    params.eval_size = int((1-params.split)*params.number)
+
+    # Define the model
+    logging.info("Creating the model...")
+    train_model_spec = model_fn('train', train_inputs, params)
+    eval_model_spec = model_fn('eval', eval_inputs, params, reuse=True)
+#%%
+    # Train the model
+    logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
+    train_and_evaluate(train_model_spec, eval_model_spec, args.model_dir, params, args.restore_from)
